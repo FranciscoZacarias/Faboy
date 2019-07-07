51b5e7ae048d12d3b1e6a05db538233eeb445555
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2019-07-07 11:44:55 +0100

    asd

--- commands/Utility/piratebay.js
+++ commands/Utility/piratebay.js
@@ -22,17 +22,19 @@ module.exports = class extends Command
         {
             let output_buf = Buffer.concat(chunks)
             let obj = (JSON.parse(output_buf.toString()))[0];
-                
+
+            console.log(parsed_message)
+
+            const desc_string = `Category: ${obj.Category} | Plataform: ${obj.Plataform} \nSeeders: ${obj.Seeders} | Leechers: ${obj.Leechers} \nSize: ${obj.Size} | Author Title: ${obj.Title}`
             const embed = new this.client.Discord.RichEmbed()
                 .setTitle("RESULTS FOR: " + parsed_message.clean_message)
                 .setDescription("REMEMBER: If you LIKE it, BUY it.")
                 .setURL(obj.URL)
                 .setColor('#FF0000')
                 .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/The_Pirate_Bay_logo.svg/2000px-The_Pirate_Bay_logo.svg.png')
-                .addField(`${obj.Name}`, `Category: ${obj.Category} | Plataform: ${obj.Plataform}`)
-                .addField(`Seeders: ${obj.Seeders} | Leechers: ${obj.Leechers}`, `Size: ${obj.Size} | Author Title: ${obj.Title}`)
-                .setFooter(`${obj.By} | ${obj.Uploaded}`)
-                //(parsed_message.args.includes('-m')) ? (pirate_embed.addField(`Magnet: `,`${obj.Magnet}`)) : ""   
+                .addField(`${obj.Name}`, desc_string)
+                .setFooter(`${obj.By} | ${obj.Uploaded}`);
+                (parsed_message.args.includes('-m')) ? (embed.addField(`Magnet: `,`${obj.Magnet}`)) : ""   
             
             return parsed_message.message.channel.send(embed);
         });
