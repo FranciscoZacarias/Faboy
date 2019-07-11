05835e3f6269d63562ba476cca935711f26224c5
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2019-07-11 23:53:56 +0100

    pasta pasta pasta

--- commands/Skyrim/100.js
+++ commands/Skyrim/100.js
@@ -10,19 +10,69 @@ module.exports = class extends Command
 
     async run(parsed_message)
     {
-        const url = `https://api.imgflip.com/caption_image?template_id=167991076&username=fzac&password=${process.env.IMGFLIP_PW}&text0=${parsed_message.clean_message}`;
-        return this.client.fetch(url,{
-            method: 'POST'
+        const { spawn } = require('child_process');
+        const script = spawn('python',['./scripts/100.py', parsed_message.clean_message]);
+        
+        const chunks = [];
+
+        script.stdout.on('data', (data) => 
+        {
+            chunks.push(data)
+        });
+        script.stdout.on('end', () => 
+        {
+            let output_buf = Buffer.concat(chunks)
+            let base64_str = (JSON.parse(output_buf.toString())).picture;
+            
+            let data = new URLSearchParams();
+            data.append('request', base64_str)
+            this.client.fetch('https://www.base64decode.net/base64-image-decoder',{
+                method: 'POST',
+                body: data
+            })
+            .then((res => res.text()))
+            .then(text =>
+            {
+                let html = JSON.stringify(text)
+                let first_char = html.indexOf('<img src=\\') + 11;
+                let uri = '';
+                let req = false;
+                while(!req)
+                {
+                    if(html[first_char] == '\\')
+                    {
+                        req = true;
+                        break;
+                    }
+                    uri += html[first_char++];
+                }
+                return parsed_message.message.channel.send(uri);
+            });
+            
+        });
+        script.stderr.on('data', (err) =>
+        {
+            this.client.logger.log(err, 'error');
+        });
+        /*
+        const uri = `https://api.imgflip.com/caption_image?template_id=167991076&username=fzac&password=${process.env.IMGFLIP_PW}&text0=${parsed_message.clean_message}&max_font_size=60&outline_color=#000000`;
+        return this.client.fetch(uri,{
+            method: 'POST',
+            header: {
+            },
+            body: {
+            }
         })
         .then((response) =>
         {
             response.json()
             .then((data) =>
             {
-                //console.log(data);
+                console.log(data);
                 if(data.success != true) return;
                 parsed_message.message.channel.send(data.data.url);
             });
         });
+        */
     }
 }
---
+++ scripts/100.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw, ImageFont
+import sys, json, base64
+from io import BytesIO
+
+try:
+	keyword = sys.argv[1]
+except:
+	keyword = "dumbfuck"
+
+image = Image.open('/Users/frank/Desktop/Work/Projetos_/Faboy/assets/100meme.png')
+font = ImageFont.truetype("impact.ttf", 60)
+draw = ImageDraw.Draw(image)
+draw.text(xy=(10,85), text=keyword, fill=(0,0,0), font=font)
+
+buffered = BytesIO()
+image.save(buffered, format="PNG")
+buffered.seek(0)
+img_bytes = buffered.read()
+
+base64_encoded_bytes = base64.b64encode(img_bytes)
+base64_encoded_str = base64_encoded_bytes.decode('ascii')
+
+print(json.dumps({'picture': base64_encoded_str}))
+sys.stdout.flush()
