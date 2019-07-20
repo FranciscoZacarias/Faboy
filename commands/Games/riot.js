b41003043ef2a0f2bb8cb1278e53f23c927a8a57
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2019-07-20 12:00:03 +0100

    this game stil trash

--- commands/Games/riot.js
+++ commands/Games/riot.js
@@ -16,8 +16,21 @@ module.exports = class extends Command
 
         const summoner = await this.get_data(`${url}/lol/summoner/v4/summoners/by-name/${parsed_message.clean_message}/${api_key}`)
         const current_match  = await this.get_data(`${url}/lol/spectator/v4/active-games/by-summoner/${summoner.id}/${api_key}`)
+        if(current_match.status)
+        {
+            return parsed_message.message.channel.send('Summoner not found or not in match');
+        }
         console.log(current_match)
 
+        const embed = new this.client.Discord.RichEmbed()
+            .setTitle("Current match for: " + summoner.name)
+
+        const players = current_match.participants
+        players.forEach(player => {
+            embed.addField("TEAM: " + player.teamId.toString().substring(0,1), player.summonerName)
+        });
+
+        return parsed_message.message.channel.send(embed);
     }
 
     async get_data(uri)
