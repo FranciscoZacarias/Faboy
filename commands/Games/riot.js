95f6b61a47aa4e380d19ccda63000723264aa914
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2019-07-20 14:31:25 +0100

    this game trash, but is workin

--- commands/Games/riot.js
+++ commands/Games/riot.js
@@ -21,14 +21,35 @@ module.exports = class extends Command
             return parsed_message.message.channel.send('Summoner not found or not in match');
         }
         console.log(current_match)
-
-        const embed = new this.client.Discord.RichEmbed()
-            .setTitle("Current match for: " + summoner.name)
-
         const players = current_match.participants
-        players.forEach(player => {
-            embed.addField("TEAM: " + player.teamId.toString().substring(0,1), player.summonerName)
-        });
+
+        let field1 = "";
+        let field2 = "";
+        for(let i = 0; i < players.length; i++)
+        {
+            const player = players[i];
+            const player_league = await (this.get_data(`${url}/lol/league/v4/entries/by-summoner/${player.summonerId}/${api_key}`));
+            if(i < 5)
+            {
+                if(!player_league[0]) 
+                    field1 += await `Summoner Name: ${player.summonerName}\n--Rank: Unranked\n`;
+                else
+                    field1 += await `Summoner Name: ${player.summonerName}\n--Rank: ${player_league[0].tier} ${player_league[0].rank}\n--LP:${player_league[0].leaguePoints} (W:${player_league[0].wins} L:${player_league[0].losses})\n`;
+            }
+            else
+            {
+                if(!player_league[0])
+                    field2 += await `Summoner Name: ${player.summonerName}\n--Rank: Unranked\n`;
+                else
+                    field2 += await `Summoner Name: ${player.summonerName}\n--Rank: ${player_league[0].tier} ${player_league[0].rank}\n--LP:${player_league[0].leaguePoints} (W:${player_league[0].wins} L:${player_league[0].losses})\n`;
+            }
+        }
+        const embed = new this.client.Discord.RichEmbed()
+            .setTitle("LEAGUE OF LEGENDS:")
+            .addField("TEAM 1", field1)
+            .addField("TEAM 2", field2)
+            .setColor("#ffffcc")
+            .setThumbnail('https://nse.gg/media/1990/lol_client_logo.png?anchor=center&mode=crop&width=300&height=300')
 
         return parsed_message.message.channel.send(embed);
     }
