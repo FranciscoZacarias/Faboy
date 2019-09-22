198d75c99060bbe8c4164bba055f2505a99840a2
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2019-09-22 14:17:22 +0100

    soudns

--- commands/Voice/speak.js
+++ commands/Voice/speak.js
@@ -6,20 +6,20 @@ module.exports = class extends Command
     constructor(name, client, locale)
     {
         super(name, client, locale);
-        this.aliases = ['join', 'youtube']
+        this.aliases = ['fala', 'diz', 'scream'];
     }
 
     async run(parsed_message)
     {
+
         let message = parsed_message.message; 
-        let link = parsed_message.clean_message;
         if(message.member.voiceChannel && !message.guild.voiceConnection)
         {
+            let audio = (parsed_message.clean_message) ? parsed_message.clean_message : 'scream';
             message.member.voiceChannel.join()
             .then(connection => 
             {
-                const stream = ytdl(message.content.split(" ")[2], {filter: 'audioonly' });
-                const dispatcher = connection.playStream(stream, { seek: 0, volume: 1 });
+                const dispatcher = connection.playFile(`./assets/audio/${audio}.mp3`, { seek: 0, volume: 1 });
                 dispatcher.on('end', () =>
                 {
                     message.guild.voiceConnection.disconnect();
