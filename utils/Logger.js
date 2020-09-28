4f4332d77224d690327fb185b74fc7441e6ecabd
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2020-09-28 17:32:40 +0100

    webhook to send me any errors

--- utils/Logger.js
+++ utils/Logger.js
@@ -1,4 +1,4 @@
-const chalk = require('chalk');
+const fs = require('fs');
 const moment = require("moment");
 
 module.exports = class Logger
@@ -6,8 +6,21 @@ module.exports = class Logger
     static log(content, type = 'log')
     {
         const logtext = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]: ${type.toUpperCase()} ${content}`;
+        
+        if(type == 'error')
+        { 
+            postErrortoWebhook("ERROR: " + content);
+        }
+
+        fs.appendFile(`./logs/${type}.log`, content + '\n', function(err){});
+        
         return console.log(logtext);
     }
 }
 
-
+function postErrortoWebhook(content) 
+{
+    const Discord = require('discord.js');
+    const webhook = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
+    webhook.send(content);
+}
