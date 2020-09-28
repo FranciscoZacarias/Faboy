2aa6e8bab63936fe4190bcd68af244734a2a5213
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2020-09-28 15:21:29 +0100

    fixed constructor

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -4,8 +4,8 @@ module.exports = class extends Command
 {
     constructor(name, client, locale)
     {
-        this.description = "Pong!";
         super(name, client, locale);
+        this.description = "Pong!";
     }
 
     async run(parsed_message)
