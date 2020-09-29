4f44d951651e672772ccee0655384d1446af11fc
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2020-09-29 20:04:57 +0100

    removed duplicate garbage with just garbage

--- Faboy.js
+++ Faboy.js
@@ -73,4 +73,59 @@ module.exports = class Faboy extends Client
             }
         });
     }
+
+    /**
+     * Meant for any command that runs /scripts/imageDraw.py
+     * @param {dictinoary} parsed_message dictonary with filtered messaage content
+     * @param {string} image image name (must be .png)
+     * @param {int} textSizeBias bias for text size
+     * @param {float} heightBias bias for text height
+     */
+    async runImageDraw(parsed_message, image, textSizeBias, heightBias)
+    {
+        const { spawn } = require('child_process');
+	
+        const script = spawn('python',['./scripts/imageDraw.py', parsed_message.clean_message, image, textSizeBias, heightBias]);
+        
+        const chunks = [];
+
+        script.stdout.on('data', (data) => { 
+		chunks.push(data)
+        });
+        script.stdout.on('end', () => 
+	    {
+            let output_buf = Buffer.concat(chunks)
+            let base64_str = (JSON.parse(output_buf.toString())).picture;
+            
+            let data = new URLSearchParams();
+            data.append('request', base64_str)
+            this.fetch('https://www.base64decode.net/base64-image-decoder',{
+                method: 'POST',
+                body: data
+            })
+            .then((res => res.text()))
+            .then(text =>
+            {
+                let html = JSON.stringify(text)
+                let tag = '<img src=\\';
+                let first_char = html.indexOf(tag) + tag.length + 1;
+                let uri = '';
+                let req = false;
+                while(!req)
+                {
+                    if(html[first_char] == '\\')
+                    {
+                        req = true;
+                        break;
+                    }
+                    uri += html[first_char++];
+                }
+                return parsed_message.message.channel.send(uri);
+            });
+        });
+        script.stderr.on('data', (err) =>
+        {
+            this.client.logger.log(err, 'error');
+        });
+    } 
 }
--- commands/Memes/stonks.js
+++ commands/Memes/stonks.js
@@ -11,52 +11,6 @@ module.exports = class extends Command
 
     async run(parsed_message)
     {
-        const { spawn } = require('child_process');
-	//Args:
-	//message to draw, image to draw on, text scale factor (higher values -> smaller text),
-	//text height factor (higher values -> lower text)
-        const script = spawn('python',['./scripts/imageDraw.py', parsed_message.clean_message, 'stonks', 3, 0.7]);
-        
-        const chunks = [];
-
-        script.stdout.on('data', (data) => 
-        {
-            chunks.push(data)
-        });
-        script.stdout.on('end', () => 
-        {
-            let output_buf = Buffer.concat(chunks)
-            let base64_str = (JSON.parse(output_buf.toString())).picture;
-            
-            let data = new URLSearchParams();
-            data.append('request', base64_str)
-            this.client.fetch('https://www.base64decode.net/base64-image-decoder',{
-                method: 'POST',
-                body: data
-            })
-            .then((res => res.text()))
-            .then(text =>
-            {
-                let html = JSON.stringify(text)
-                let tag = '<img src=\\';
-                let first_char = html.indexOf(tag) + tag.length + 1;
-                let uri = '';
-                let req = false;
-                while(!req)
-                {
-                    if(html[first_char] == '\\')
-                    {
-                        req = true;
-                        break;
-                    }
-                    uri += html[first_char++];
-                }
-                return parsed_message.message.channel.send(uri);
-            });
-        });
-        script.stderr.on('data', (err) =>
-        {
-            this.client.logger.log(err, 'error');
-        });
+        this.client.runImageDraw(parsed_message, 'stonks', 3, 0.7);
     }
 }
